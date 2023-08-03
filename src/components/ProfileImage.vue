8db63f1bd7b42480bcde54bbfa85c11e720a75bc
amira <fathallah_amira@yahoo.fr>
2023-08-03 22:36:33 +0200

    LAB week 10 Done !

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({image:String});
+</script>
+
+<template>
+<!-- src="https://i.imgur.com/9yw1Fyw.jpg" -->
+<img
+      :src="image"
+      class="profile"
+      alt="profile"
+    />
+</template>
+
+<style>
+
+</style>
---
+++ src/components/Timestamp.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps({time:String});
+</script>
+
+<template>
+<span class="timestamp">{{time}}</span>
+</template>
+
+<style>
+
+</style>
